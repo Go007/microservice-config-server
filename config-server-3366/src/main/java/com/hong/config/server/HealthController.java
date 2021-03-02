05a3117e13a959ee50c5a157095bb01a5f5a4fe9
wanghong <htimeis007money>
2021-03-02 15:08:33 +0800

    init

--- config-server-3366/src/main/java/com/hong/config/server/HealthController.java
+++ config-server-3366/src/main/java/com/hong/config/server/HealthController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ *  Spring Boot Actuator:健康检查，审计，统计和监控
+ */
 @RestController
 public class HealthController {
 
